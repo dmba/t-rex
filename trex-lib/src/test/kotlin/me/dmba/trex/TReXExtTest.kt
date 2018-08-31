a9d44f44439fffa01744ff7d22e1c380ad4c65c5
Dmytro Babiienko <dmitry.babienko@gmail.com>
2018-08-31 16:46:33 +0200

    Add trex lib extensions tests

---
+++ trex-lib/src/test/kotlin/me/dmba/trex/TReXExtTest.kt
@@ -0,0 +1,20 @@
+package me.dmba.trex
+
+import org.junit.Test
+
+/**
+ * Test class for extension functions from [TReXExt]
+ */
+class TReXExtTest {
+
+    @Test
+    fun `test with for observable`() {
+
+    }
+
+    @Test
+    fun `test with for completable`() {
+
+    }
+
+}
